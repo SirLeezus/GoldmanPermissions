acd7279753ae7a9af90d5aa982d9af3ba14deb74
Lee <leecook808@gmail.com>
2023-08-29 12:43:17 -0700

    Fix

--- src/main/java/lee/code/permissions/listeners/QuitListener.java
+++ src/main/java/lee/code/permissions/listeners/QuitListener.java
@@ -5,6 +5,8 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
 
+import java.util.UUID;
+
 public class QuitListener implements Listener {
   private final Permissions permissions;
 
@@ -14,6 +16,8 @@ public class QuitListener implements Listener {
 
   @EventHandler
   public void onPlayerQuit(PlayerQuitEvent e) {
-    permissions.getPermissionManager().deleteCommandData(e.getPlayer().getUniqueId());
+    final UUID uuid = e.getPlayer().getUniqueId();
+    permissions.getStaffChatManager().removeChatter(uuid);
+    permissions.getPermissionManager().deleteCommandData(uuid);
   }
 }
