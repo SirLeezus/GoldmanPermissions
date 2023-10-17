09e059a4be01d82a56f1d6f3675f0dc7cf4f0651
Lee <leecook808@gmail.com>
2023-10-16 17:39:27 -0700

    remove god rank

--- src/main/java/lee/code/permissions/enums/RankData.java
+++ src/main/java/lee/code/permissions/enums/RankData.java
@@ -10,10 +10,9 @@ public enum RankData {
   DEFAULT(null, null, "z", ChatColor.YELLOW, false),
   LEGEND("&e[&6&lLegend&e]", null, "y", ChatColor.GOLD, false),
   ELITE("&d[&5&lElite&d]", null, "x", ChatColor.DARK_PURPLE, false),
-  GOD("&a[&2&lGod&a]", null, "w", ChatColor.DARK_GREEN, false),
-  MOD("&d[&5&lMod&d]", null, "v", ChatColor.DARK_PURPLE, true),
-  ADMIN("&a[&2&lAdmin&a]", null, "u", ChatColor.DARK_GREEN, true),
-  OWNER("&4[&c&lOwner&4]", null, "t", ChatColor.RED, true);
+  MOD("&d[&5&lMod&d]", null, "c", ChatColor.DARK_PURPLE, true),
+  ADMIN("&a[&2&lAdmin&a]", null, "b", ChatColor.DARK_GREEN, true),
+  OWNER("&4[&c&lOwner&4]", null, "a", ChatColor.RED, true);
 
   @Getter private final String prefix;
   @Getter private final String suffix;
